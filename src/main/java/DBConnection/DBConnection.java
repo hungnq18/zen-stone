1c3f6cc7bbabc0e3a47ab6a6dcb87160074c3ae6
hungnq10 <nguyenhungpes18@gmail.com>
2025-06-26 23:10:01 +0700

    add full

--- src/main/java/DBConnection/DBConnection.java
+++ src/main/java/DBConnection/DBConnection.java
@@ -11,26 +11,34 @@ import java.sql.SQLException;
 public class DBConnection {
     
     public static Connection connect() {
-        try { 
+        try {
+            System.out.println("[DBConnection] Loading JDBC driver...");
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            
+            System.out.println("[DBConnection] JDBC driver loaded.");
+
             // Thông tin Azure SQL fix cứng
-            String dbHost = "zen-stone.database.windows.net";
+            String dbHost = "zenstone.database.windows.net";
             String dbName = "zen-stone";
-            String dbUser = "adminuser@zenstone"; // Thay bằng username thực tế
+            String dbUser = "adminuser"; // Thay bằng username thực tế
             String dbPassword = "Hung18102003";    // Thay bằng password thực tế
 
             String urlConnect = String.format(
                 "jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;",
                 dbHost, dbName, dbUser, dbPassword
             );
-
-            // tao doi tuong connection 
+            System.out.println("[DBConnection] Connection string: " + urlConnect);
+            System.out.println("[DBConnection] Attempting to connect to database...");
             Connection conn = DriverManager.getConnection(urlConnect);
+            System.out.println("[DBConnection] Connection successful!");
             return conn;
-        } catch(ClassNotFoundException | SQLException ex) {
-            System.out.println(ex);
-        } 
+        } catch(ClassNotFoundException ex) {
+            System.out.println("[DBConnection] JDBC Driver not found: " + ex);
+        } catch(SQLException ex) {
+            System.out.println("[DBConnection] SQL Exception: " + ex);
+        } catch(Exception ex) {
+            System.out.println("[DBConnection] General Exception: " + ex);
+        }
+        System.out.println("[DBConnection] Connection failed!");
         return null;
     }
 }
