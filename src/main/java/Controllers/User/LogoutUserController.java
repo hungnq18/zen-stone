9b5731bbb1a801f91786d5fa93cfa3ec2b831bec
hungnq10 <nguyenhungpes18@gmail.com>
2025-07-04 00:22:03 +0700

    fix deploy

--- src/main/java/Controllers/User/LogoutUserController.java
+++ src/main/java/Controllers/User/LogoutUserController.java
@@ -2,14 +2,12 @@ package Controllers.User;
 
 import Util.Authentication;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet("/logout")
 public class LogoutUserController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
